eaaee7856ffd7d476f2e5abbbb3c72ad86558c83
Arth Patel <76816540+Arthpatel01@users.noreply.github.com>
2023-10-05 00:01:48 +0530

    Update settings.py

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@n36er!^rnnjauxnx3d3qiv9()@-6p4c3xe_*!#e0g7d!%c8$0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*", "https://artheverywhere.pythonanywhere.com", "artheverywhere.pythonanywhere.com", ".vercel.app"]
 
