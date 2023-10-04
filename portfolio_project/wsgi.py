7fb62efb12725045d24bad0db7aaff5ab59fcf2a
Arth Patel <76816540+Arthpatel01@users.noreply.github.com>
2023-10-04 23:54:05 +0530

    Update wsgi.py

--- portfolio_project/wsgi.py
+++ portfolio_project/wsgi.py
@@ -1,7 +1,7 @@
 """
 WSGI config for portfolio_project project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+It exposes the WSGI callable as a module-level variable named ``app``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_project.settings')
 
 app = get_wsgi_application()
+handler = app
+
