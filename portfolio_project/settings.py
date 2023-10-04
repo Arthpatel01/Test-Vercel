0bee952760dc16216161c7f3e069c8ef069aabcc
Arth Patel <76816540+Arthpatel01@users.noreply.github.com>
2023-10-04 23:47:28 +0530

    Update settings.py

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@n36er!^rnnjauxnx3d3qiv9()@-6p4c3xe_*!#e0g7d!%c8$0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*", "https://artheverywhere.pythonanywhere.com", "artheverywhere.pythonanywhere.com"]
+ALLOWED_HOSTS = ["*", "https://artheverywhere.pythonanywhere.com", "artheverywhere.pythonanywhere.com", ".vercel.app"]
 
 # Application definition
 
@@ -66,7 +66,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'portfolio_project.wsgi.application'
+WSGI_APPLICATION = 'portfolio_project.wsgi.app'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
